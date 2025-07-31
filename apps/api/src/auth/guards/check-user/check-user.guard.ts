f3a172f602762cd1f65453b3a4e6ab38e632c168
Jamal Mohafil <109922894+JamalMohafil@users.noreply.github.com>
2025-07-31 11:45:02 +0300

    Update check-user.guard.ts

--- apps/api/src/auth/guards/check-user/check-user.guard.ts
+++ apps/api/src/auth/guards/check-user/check-user.guard.ts
@@ -1,49 +1,41 @@
-// optional-jwt-auth.guard.ts
-import { Injectable, ExecutionContext } from '@nestjs/common';
+ import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class CheckUserGuard extends AuthGuard('check-user') {
-  // تجاوز دالة canActivate لمعالجة الخطأ عند عدم وجود توكن
-  canActivate(
+   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    // استخدام نمط التحقق من النوع قبل استدعاء .catch
+ 
     const result = super.canActivate(context);
 
-    // إذا كانت النتيجة وعداً (Promise)
-    if (result instanceof Promise) {
+     if (result instanceof Promise) {
       return result.catch((error) => {
-        // إذا كان الخطأ بسبب عدم وجود توكن
-        if (error.message === 'No auth token') {
-          // تعيين req.user إلى null
-          const request = context.switchToHttp().getRequest();
+         if (error.message === 'No auth token') {
+           const request = context.switchToHttp().getRequest();
           request.user = null;
-          // السماح بالمرور
-          return true;
+           return true;
         }
-        // إعادة رمي الخطأ إذا كان خطأ آخر
-        throw error;
+         throw error;
       });
     }
-
-    // إذا كانت النتيجة Observable
+ 
     if (result instanceof Observable) {
       return new Observable((subscriber) => {
         result.subscribe({
           next: (value) => subscriber.next(value),
           error: (error) => {
-            // إذا كان الخطأ بسبب عدم وجود توكن
+     
             if (error.message === 'No auth token') {
-              // تعيين req.user إلى null
+             
               const request = context.switchToHttp().getRequest();
               request.user = null;
-              // السماح بالمرور
+        
               subscriber.next(true);
               subscriber.complete();
             } else {
-              // إرسال الخطأ إذا كان خطأ آخر
+        
               subscriber.error(error);
             }
           },
@@ -52,13 +44,11 @@ export class CheckUserGuard extends AuthGuard('check-user') {
       });
     }
 
-    // إذا كانت النتيجة boolean، نرجعها كما هي
+ 
     return result;
   }
 
-  // تجاوز دالة handleRequest للتأكد من عدم رمي استثناء
-  handleRequest(err: any, user: any, info: any) {
-    // عدم رمي استثناء إذا لم يكن هناك مستخدم
-    return user;
+   handleRequest(err: any, user: any, info: any) {
+     return user;
   }
 }
